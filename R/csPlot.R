06882e9863e9cad777b1b14078701d9dac64d113
Angelos Krypotos <amkrypotos@gmail.com>
2016-01-25 21:45:25 +0100

    Fixed bugg with names.arg

--- R/csPlot.R
+++ R/csPlot.R
@@ -81,7 +81,7 @@ csPlot <- function(cs1, cs2, group = NULL, data = NULL,
     print(meanz)
     print(sdz)
     graphics::barplot(base::matrix(base::unlist(meanz), 2, 2), col = col,
-                      beside = T, names.arg = "", ...)
+                      beside = T, names.arg = NULL, ...)
     graphics::legend(x = "topright", legend = legend, inset=c(-0.2,0),
                      title = "Stimulus", bty = "n", pch = 15,
                      bg = "black", col = col, cex = 1.5)
