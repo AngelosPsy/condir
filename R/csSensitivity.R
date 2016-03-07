790b324477f98af5df39b1be57aaa33dba9e3a58
Angelos Krypotos <amkrypotos@gmail.com>
2016-03-07 21:24:27 +0100

    Implement outlier removal

--- R/csSensitivity.R
+++ R/csSensitivity.R
@@ -12,6 +12,8 @@
 #' @param conf.level confidence level of the interval.
 #' @param mu a numeric value for the mean value or mean difference
 #' @param rscaleSens the scale factor for the prior used in the Bayesian t.test
+#' @param out.thres The threeshold for detecting outliers (default is 3). If set
+#' to 0, no outliers analysis is performed.
 #' @details
 #' \code{csCompare} performs both a student t-test (using the
 #' \code{stats::t.test} function) and a Bayesian t-test (using the
@@ -33,12 +35,13 @@
 #' @export
 csSensitivity <- function(cs1, cs2, group = NULL, data = NULL,
                           alternative = "two.sided", conf.level = 0.95, mu = 0,
-                          rscaleSens = c("medium", "wide", "ultrawide")){
+                          rscaleSens = c(.707, 1, 1.41),
+                          out.thres = 3){
 
   # Extract t statistic
   ftt <- condir::csCompare(cs1 = cs1, cs2 = cs2, group = group, data = data,
                   alternative = alternative, conf.level = conf.level, mu = mu,
-                  descriptives = TRUE, boxplot = FALSE)
+                  descriptives = TRUE, boxplot = FALSE, out.thres = out.thres)
 
   # Need to define the number of participants for each group
   paired <- ifelse(is.null(group), TRUE, FALSE)
@@ -62,18 +65,65 @@ csSensitivity <- function(cs1, cs2, group = NULL, data = NULL,
                                                          simple = FALSE))
   colnames(btt) <- rscaleSens
   # Structure results to a data frame so as to be easier to read
-  res <- matrix(-999, nrow = length(rscaleSens), ncol = 8)
-  colnames(res) <- c("nG1", "nG2", "LNI", "HNI", "rscale", "bf10",
+  res.mat <- matrix(-999, nrow = length(rscaleSens), ncol = 8)
+  colnames(res.mat) <- c("nG1", "nG2", "LNI", "HNI", "rscale", "bf10",
                            "bf01", "propError")
 
   for (i in 1:length(rscaleSens)){
-    res[i, ] <- c(nG1 = n1, nG2 = n2, LNI = as.character(ftt$bayes.res$LNI),
+    res.mat[i, ] <- c(nG1 = n1, nG2 = n2, LNI = as.character(ftt$bayes.res$LNI),
     HNI = as.character(ftt$bayes.res$HNI), rscale = rscaleSens[i],
     bf10 = exp(btt[, i][["bf"]]),
     bf01 = 1/exp(btt[, i][["bf"]]), propError = btt[, i]$properror)
   }
+  res <- list()
+  res$res <- data.frame(res.mat)
+  # Outlier analysis
+  if (out.thres != 0){
+    if (paired){
+      cout <- cs1 - cs2
+      ### BUGG
+      outz <- stats::rstandard(stats::lm(cout~1))
+      out.HCI <- which(outz > out.thres)
+      out.LCI <- which(outz < (-out.thres))
+      if (length(out.HCI) == 0 && length(out.LCI) == 0){
+        out.present <- FALSE
+      } else {
+        out.present <- TRUE
+        cs1.out <- cs1[-c(out.HCI, out.LCI)]
+        cs2.out <- cs2[-c(out.HCI, out.LCI)]
+        res.out <- condir::csSensitivity(cs1 = cs1.out, cs2 = cs2.out, group = NULL,
+                                 data = NULL, alternative = alternative,
+                                 conf.level = conf.level, mu = mu,
+                                 rscaleSens = rscaleSens, out.thres = 0)
+      }
 
-  res = data.frame(res)
+    } else {
+      cout <- cs3
+      outz <- stats::rstandard(stats::lm(cout~group))
+      out.HCI <- which(outz > out.thres)
+      out.LCI <- which(outz < (-out.thres))
+      cs1.out <- cs1[-c(out.HCI, out.LCI)]
+      cs2.out <- cs2[-c(out.HCI, out.LCI)]
+      group.out <- group[-c(out.HCI, out.LCI)]
 
+      if (length(out.HCI) == 0 && length(out.LCI) == 0){
+        out.present <- FALSE
+      } else {
+        out.present <- TRUE
+        compare.out <- condir::csSensitivity(cs1 = cs1.out, cs2 = cs2.out,
+                                 group = group.out, data = data,
+                                 alternative = alternative,
+                                 conf.level = conf.level, mu = mu,
+                                 rscaleSens = rscaleSens, out.thres = 0)
+
+      }
+    }
+  } else {
+    # This is the case you do not want outliers to be detected
+    out.present = FALSE
+  }
+  if (out.present){
+    res$res.out = res.out[[1]]
+  }
   return(res)
 }
