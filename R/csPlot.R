f0a93d5286d9ff1d333a0f33fb7659db5987aab4
AngelosPsy <amkrypotos@gmail.com>
2016-09-07 17:50:17 +0200

    Added xpd argument in legend

--- R/csPlot.R
+++ R/csPlot.R
@@ -94,6 +94,6 @@ csPlot <- function(cs1, cs2, group = NULL, data = NULL, ylab = "CRs",
     graphics::mtext(ylab, 2, line = 3, cex = 1.5, las=0)
     graphics::legend(x = "topright", legend = legend, inset=c(-0.2, 0),
                      title = "Stimulus", bty = "n", pch = 15,
-                     bg = "black", col = col, cex = 1.5)
+                     bg = "black", col = col, cex = 1.5, xpd = TRUE)
   }
 }
