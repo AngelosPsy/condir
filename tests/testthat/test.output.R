1ef87e7b94a972be60685360176c1dd2d5c43f10
Angelos Krypotos <amkrypotos@gmail.com>
2020-11-25 11:26:42 +0100

    Did test forcsRobustnessPlot that was failing on CRAN

---
+++ tests/testthat/test.output.R
@@ -0,0 +1,7 @@
+context("Functions for statistical tests")
+
+test_that("plots", {
+  set.seed(1000)
+  vdiffr::expect_doppelganger("csRobPlot",  csRobustnessPlot(cs1 = rnorm(n = 100, mean = 10),
+                                                             cs2 = rnorm(n = 100, mean = 9)))
+})
