ddf375a3e271c2e42b630f09438b0029a167a1d8
Angelos Krypotos <amkrypotos@gmail.com>
2020-11-25 17:00:05 +0100

    testthat updates

--- tests/testthat/test.output.R
+++ tests/testthat/test.output.R
@@ -2,6 +2,5 @@ context("Functions for statistical tests")
 
 test_that("plots", {
   set.seed(1000)
-  vdiffr::expect_doppelganger("csRobPlot",  csRobustnessPlot(cs1 = rnorm(n = 100, mean = 10),
-                                                             cs2 = rnorm(n = 100, mean = 9)))
+  vdiffr::expect_doppelganger("csRobPlot",  csRobustnessPlot(cs1 = rnorm(n = 100, mean = 10), cs2 = rnorm(n = 100, mean = 9)))
 })
